9197296d9d34c30e54ab98007593ba46bc213fe3
Vicky <ying.vicky.cao@outlook.com>
2019-09-19 00:08:10 +0800

    add autils

---
+++ app/src/main/java/com/yingvickycao/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yingvickycao.test;
+
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+
+        setContentView(R.layout.main_layout);
+    }
+}
